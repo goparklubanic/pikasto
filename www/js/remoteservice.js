ca7d70a735b4bf83832cae49e5bc14d7e85b08ff
goparklubanic <ngoparios@yahoo.com>
2018-03-21 09:31:14 +0700

    chante remote service

--- www/js/remoteservice.js
+++ www/js/remoteservice.js
@@ -1,4 +1,5 @@
-var serviceUrl = 'http://localhost/ngh/pikasto/waiter/index.php';
+//var serviceUrl = 'http://localhost/ngh/pikasto/waiter/index.php';
+var serviceUrl = 'https://pikas.klubaners.web.id/waiter/index.php';
 
 $(document).on('click','#home',function(){
   window.location='./index.html';
